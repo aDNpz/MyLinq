eda463228bffbd34cc98879bee85bce0df82406b
KD200344 <KD200344@WS10A24>
2021-09-30 18:26:25 +0200

    Add project files.

---
+++ ClassLibrary1/IEnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyLinq.Logic
+{
+    public static class IENumerableExtensions
+    {
+
+    }
+}
---
+++ MyLinq.Logic.UnitTest/IEnumerableExtensionUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLinq.Logic.UnitTest
+{
+    [TestClass]
+    public class IEnumerableExtensionUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
